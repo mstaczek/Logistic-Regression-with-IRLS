3cff557fff6d04bf4b68263588e4e337cb8426a2
mstaczek <matemmatem2000@gmail.com>
2023-04-04 14:01:19 +0200

    Add LR IRLS to experiment

--- LogisticRegressionIRLS.py
+++ LogisticRegressionIRLS.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import scipy
 
 class LR():
     def __init__(self, maximisation_minorisation = False):
@@ -8,19 +9,19 @@ class LR():
         self.maximisation_minorisation = maximisation_minorisation
         
     def sigmoid(self, X, β):
-        return 1/(1+np.exp(X @ β))
+        return scipy.special.expit(X @ β)
 
-    def fit(self, X, Y, iter_max = 100, interactions = [], l2=0): 
+
+    def fit(self, X, Y, iter_max = 100, interactions = [], l2=0.05): 
         
-        # X- DataFrame of predictors
+        # X- matrix of predictors
         # Y- matrix of target variable of shape N x 1
         
         # iter_max- maximum number of iterations 
         # interactions- a matrix with two columns, in which each row specifies a pair of variables between which we want to consider interactions
         # l2- ridge regularization strength
-        
-        
-        X=np.c_[np.ones(X.shape[0]) , X.values]
+        Y = Y.reshape(-1,1)
+        X=np.c_[np.ones(X.shape[0]) , X]
         self.interactions = interactions
         for interaction in interactions:
             X=np.c_[X, X[:,interaction[0]+1] * X[:,interaction[1]+1]]
@@ -46,8 +47,8 @@ class LR():
             print("Algorithm didn't converge! Iteration limit reached!")
 
     def predict(self, X, p=1/2):
-        X_exp = np.c_[np.ones(X.shape[0]) , X.values]
+        X_exp = np.c_[np.ones(X.shape[0]) , X]
         for interaction in self.interactions:
             X_exp=np.c_[X_exp, X_exp[:,interaction[0]+1] * X_exp[:,interaction[1]+1]]
-        return (self.sigmoid(X_exp,self.beta)>p).astype(int)
+        return (self.sigmoid(X_exp,self.beta)<p).astype(int)
     
